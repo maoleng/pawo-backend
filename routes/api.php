c9dd8beb3e3ab28f5ba7e354bc0bcca6f4bd600a
Bùi Hữu Lộc <feature451@gmail.com>
2023-08-25 10:02:29 +0700

    feat: auth

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,13 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\ApiAuthenticate;
 use Illuminate\Support\Facades\Route;
 
+Route::get('authed', function () {
+    return [
+        'status' => true,
+        'data' => c('authed'),
+    ];
+});
+
 Route::group(['prefix' => 'job'], function () {
     Route::get('/', [JobController::class, 'index']);
     Route::get('/{id}', [JobController::class, 'show']);
